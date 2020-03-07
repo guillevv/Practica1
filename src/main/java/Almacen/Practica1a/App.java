e8e0b81486bf328d78e580a20c596644d015ed61
Guillermo Ventas <56960692+Guilleuve@users.noreply.github.com>
2020-03-07 20:21:04 +0100

    primer

---
+++ src/main/java/Almacen/Practica1a/App.java
@@ -0,0 +1,13 @@
+package Almacen.Practica1a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
